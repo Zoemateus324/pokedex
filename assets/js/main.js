8646973cc7f70a2b6c746b86739164fc21ba3a9d
valcucinebrasil <102249283+valcucinebrasil@users.noreply.github.com>
2023-04-05 10:08:41 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,14 +4,14 @@ const url = `https://pokeapi.co/v2/pokemon?offset=${offset}&limit=${limit}`
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-      <span class="number">#001</span>
-      <span class="name">${pokemon.name}</span>
-      <div class="detail">
-        <ol class="type">
-            <li class"type">grass</li>
-            <li class="type">poison</li>
-        </ol>
-        <img src="https://raw.githubsercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+                <span class="number">#001</span>
+                <span class="name">${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        <li class="type">grass</li>
+                        <li class="type">Electric</li>
+                    </ol>
+        <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
       </div>
     </li> 
  `
