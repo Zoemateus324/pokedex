f18204c27ccce108c92ad518e7ff5165e1685a2d
valcucinebrasil <102249283+valcucinebrasil@users.noreply.github.com>
2023-04-06 15:36:13 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,30 @@
 const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/v2/pokemon?offset=${offset}&limit=${limit}`
+
+function convertPokemonTypesToLi(pokemonTypes){
+     return pokemon.Types.map((typeSlot)=>'<li class="type">${typeSlot.type.name}</li>')
+}
+
+
+
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-                <span class="number">#001</span>
+                <span class="number">#${pokemon.order}</span>
                 <span class="name">${pokemon.name}</span>
+                
                 <div class="detail">
                     <ol class="types">
-                        <li class="type">grass</li>
+                    <li class="type">
+                        ${convertPokemonTypesToLi(pokemon.types).join('')}
+                        </li>
                         <li class="type">Electric</li>
                     </ol>
         <img src="https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
-      </div>
+    </div>
     </li> 
- `
+`
 }
 
 const pokemonList = document.getElementById('pokemonList')
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,19 +1,20 @@
 const pokeApi = {}
-pokeApi.getPokemonDetail= (pokemon)=>{
+pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-        .then((response)=>response.json())
+        .then((response) => response.json())
 }
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
         .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests)=> Promise.all(detailRequests))
-        .then((pokemonsDetails)=>{
-            debugger
-            console.log(pokemonsDetails)
-        })
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemons) => pokemonsDetails)
+        
 }
 
